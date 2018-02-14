a9082d3879647ab54ee06632b74c6dc30d2bbeab
Druxa063 <andru-163@mail.ru>
2018-02-14 20:50:27 +0400

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * GKislin
@@ -23,13 +23,44 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        List<UserMealWithExceed> list = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
-        return null;
+        int dayOfMonth = 0;
+        int sumCalories = 0;
+        Map<LocalDate, Boolean> limitCalories = new HashMap<>(); //Key day, Value exceed;
+        List<UserMealWithExceed> listFilter = new ArrayList<>();
+        for (UserMeal userMeal : mealList) {
+            if(dayOfMonth == 0) dayOfMonth = userMeal.getDateTime().getDayOfMonth();
+            if(dayOfMonth == userMeal.getDateTime().getDayOfMonth()) {
+                sumCalories += userMeal.getCalories();
+            } else {
+                sumCalories = userMeal.getCalories();
+                dayOfMonth = userMeal.getDateTime().getDayOfMonth();
+            }
+            if(sumCalories > caloriesPerDay) {
+                limitCalories.put(userMeal.getDateTime().toLocalDate(), true);
+            }
+            else {
+                limitCalories.put(userMeal.getDateTime().toLocalDate(), false);
+            }
+        }
+        for (UserMeal userMeal : mealList) {
+            if(TimeUtil.isBetween(userMeal.getDateTime().toLocalTime(), startTime, endTime)){
+                LocalDateTime localDateTime = userMeal.getDateTime();
+                String description = userMeal.getDescription();
+                int calories = userMeal.getCalories();
+                boolean exceed = limitCalories.get(localDateTime.toLocalDate());
+                listFilter.add(new UserMealWithExceed(localDateTime, description, calories, exceed));
+            }
+        }
+        return listFilter;
     }
 }
