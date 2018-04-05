d340f743a4d5a39e8dedafd58fa693056b53573e
Druxa063 <andru-163@mail.ru>
2018-04-05 19:37:27 +0400

    HW05

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,7 +1,9 @@
 package ru.javawebinar.topjava.web;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 
--- src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
+++ src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
@@ -2,11 +2,13 @@ package ru.javawebinar.topjava;
 
 import org.springframework.test.context.ActiveProfilesResolver;
 
+import static ru.javawebinar.topjava.Profiles.*;
+
 //http://stackoverflow.com/questions/23871255/spring-profiles-simple-example-of-activeprofilesresolver
 public class ActiveDbProfileResolver implements ActiveProfilesResolver {
 
     @Override
     public String[] resolve(Class<?> aClass) {
-        return new String[]{Profiles.getActiveDbProfile()};
+        return new String[]{getActiveDbProfile(), REPOSITORY_IMPLEMENTATION};
     }
 }
